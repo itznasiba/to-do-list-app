b8ec97bbdad9c4077fe898bd9c4d4e6badfea77c
itznasiba <zeynallinesibe2006@gmail.com>
2026-03-26 22:18:31 +0400

    updated migration

--- basic-authentication/Program.cs
+++ basic-authentication/Program.cs
@@ -25,8 +25,16 @@ namespace basic_authentication
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                var context = services.GetRequiredService<ApplicationDbContext>();
-                context.Database.Migrate();
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
             }
 
             // Configure the HTTP request pipeline.
